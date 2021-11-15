c0678d9d193d5db67529e087b6ae9672696b6747
Im-HyeJeong <dlaowjde@naver.com>
2021-11-15 15:52:58 +0900

    first commit

---
+++ HANDSONPROJECT/src/main/java/CAU/HANDSONPROJECT/HandsonprojectApplication.java
@@ -0,0 +1,13 @@
+package CAU.HANDSONPROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsonprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsonprojectApplication.class, args);
+	}
+
+}
